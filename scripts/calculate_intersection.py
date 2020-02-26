5ef2f344680da5152c87437519db26682da26e46
antic11d <dimitrije.misa@gmail.com>
2020-02-26 22:27:01 +0100

    Finalized intersection script

--- scripts/calculate_intersection.py
+++ scripts/calculate_intersection.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def intersection(intersect, lst2):
     return set([value for value in lst2 if value in intersect])
 
@@ -12,7 +15,22 @@ def list_intersection(list_of_columns):
 
 
 def main():
-    
+    columns = []
+    for i in range(1, 5):
+        path = f'../data/group_{i}/columns_{i}.csv'
+        df = pd.read_csv(path)
+        cols = ['Unnamed: 0'] + list(df.columns)
+        columns.append(cols)
+
+    columns_intersected = list_intersection(columns)
+
+    for i in range(1, 5):
+        print(f'group_{i}')
+        path = f'../data/group_{i}/data.csv'
+        df = pd.read_csv(path)
+        intersected = df[list(columns_intersected)]
+        print(f'\tShape after intersection: {intersected.shape}')
+        intersected.to_csv(f'../data/group_{i}/data_intersected.csv', index_label='Unnamed: 0', index=False)
 
 
 if __name__ == '__main__':
