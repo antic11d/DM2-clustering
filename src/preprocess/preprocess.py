e5ab1d2c57f8fdd6a1eabaa7e5ab3915a53c1bf8
antic11d <dimitrije.misa@gmail.com>
2020-07-08 19:01:39 +0200

    Folder structure and preprocessing refactored

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import re
 import csv
+import argparse
 
 # Relative path to folder containing samples
 prefix = '../../Projekat_1/'
@@ -18,17 +19,12 @@ def join(p1, p2):
     return os.path.join(p1, p2)
 
 
-def get_filename(folder):
-    path = join(prefix, folder)
+def get_filename(data_path, folder):
+    path = join(data_path, folder)
     csv = [file for file in os.listdir(path) if file.endswith('.csv')]
     filename = csv[0]
     return filename
 
-
-common_human_list = pd.read_csv(join(prefix, 'common_human_list.csv'))
-genome_sample = pd.read_csv(join(prefix, 'SCT-10x-Metadata_readylist_merged-PBMC-tasks-short-Bgd.csv'))
-
-ENSG_IDs = list(common_human_list['ENSG_ID'])
 
 
 def transpose(data, sample):
@@ -42,9 +38,9 @@ def new_index(shape, sample):
     return index
 
 
-# Dropping ENSG_IDs which are not ni common_human_list
-def drop_invalid_ensg(df):
-    return df[df['Index'].isin(ENSG_IDs)]
+# Dropping ensg_ids which are not ni common_human_list
+def drop_invalid_ensg(df, ensg_ids):
+    return df[df['Index'].isin(ensg_ids)]
 
 
 # Dropping genes after transposing
@@ -66,49 +62,38 @@ def drop_cells(df, threshold_1, threshold_2):
     df.drop(columns=dropping, inplace=True)
 
 
-def prepare(sample):
+def prepare(data_path, sample, threshold_1, threshold_2, ensg_ids):
     print(f'\tParsing {sample}')
-    data = pd.read_csv(join(prefix, sample) + '/' + get_filename(sample))
+    data = pd.read_csv(join(data_path, sample) + '/' + get_filename(data_path, sample))
 
-    data = drop_invalid_ensg(data)
+    data = drop_invalid_ensg(data, ensg_ids)
 
-    drop_cells(data, 500, 1000)
+    drop_cells(data, threshold_1, threshold_2)
 
     data = transpose(data, sample)
 
     return data
 
 
-def prepare_genome_sample():
+def prepare_genome_sample(path):
+    genome_sample = pd.read_csv(join(path, 'SCT-10x-Metadata_readylist_merged-PBMC-tasks-short-Bgd.csv'))
     genome_values = genome_sample['GENOME']
     sample_keys = genome_sample['SAMPLE']
 
     return dict(zip(sample_keys, genome_values))
 
 
-def test_mapping(sample_to_genome_mapping):
-    for i, group in enumerate(groups):
-        print('Group: {}'.format(i))
-        found_genomes = set([])
-        for gsm in group:
-            genome_value = sample_to_genome_mapping[gsm]
-            print(genome_value)
-            found_genomes.add(genome_value)
-            if len(found_genomes) > 2:
-                print('Unable to join group{}'.format(i))
-                break
+def prepare_genome_to_human_map(data_path):
+    common_human_list = pd.read_csv(join(data_path, 'common_human_list.csv'))
+    ENSG_IDs = list(common_human_list['ENSG_ID'])
 
-    print('Able to map')
-
-
-def prepare_genome_to_human_map():
     keys = common_human_list['ENSG_ID']
     mapping_columns = ['hg19', 'hg38', 'Ensembl_GRCh38.p12_rel94']
     maps = []
     for column in mapping_columns:
         maps.append(dict(zip(keys, common_human_list[column])))
 
-    return maps
+    return maps, ENSG_IDs
 
 
 # ensg0000123456_#LYL1 ide u
@@ -125,9 +110,17 @@ def map_genome_to_human(columns, maps, genome_to_human_csv_mapping, genome_value
 
 
 def main():
-    sample_to_genome_mapping = prepare_genome_sample()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', help='Absolute path for the original data file')
+    parser.add_argument('--save_path', help='Absolute save path for the preprocessed file')
+    parser.add_argument('--cell_thresh1', help='Threshold for cells - minimum of positive values', type=float)
+    parser.add_argument('--cell_thresh2', help='Threshold for cells - minimum sum', type=float)
+    parser.add_argument('--gene_drop', help='Threshold for genes', type=float)
+    args = parser.parse_args()
 
-    maps = prepare_genome_to_human_map()
+    sample_to_genome_mapping = prepare_genome_sample(args.data_path)
+
+    maps, ensg_ids = prepare_genome_to_human_map()
     genome_to_human_csv_mapping = {
         'hg19': 2,
         'hg38': 2,
@@ -139,25 +132,25 @@ def main():
 
         group_id = f'group_{i + 1}'
         print(group_id)
-        dfs = [prepare(sample) for sample in group]
+        dfs = [prepare(args.data_path, sample, args.cell_thresh1, args.cell_thresh2, ensg_ids) for sample in group]
 
         resulting = pd.concat(dfs, sort=False)
-        drop_genes(resulting, 1.0)
+        drop_genes(resulting, args.gene_drop)
 
         genome_value = sample_to_genome_mapping[group[0]]
 
         resulting.columns = map_genome_to_human(resulting.columns, maps, genome_to_human_csv_mapping, genome_value)
         print(f'\tShape after parsing: {resulting.shape}')
 
-        os.mkdir(join('../data', group_id))
+        os.mkdir(join(args.save_path, group_id))
 
-        print(f'\tSaving columns: ../data/{group_id}/columns_{i+1}.csv')
+        print(f'\tSaving columns: {args.save_path}/{group_id}/columns_{i+1}.csv')
 
-        out = csv.writer(open(f'../data/{group_id}/columns_{i+1}.csv', "w"), delimiter=',', quoting=csv.QUOTE_ALL)
+        out = csv.writer(open(f'{args.save_path}/{group_id}/columns_{i+1}.csv', "w"), delimiter=',', quoting=csv.QUOTE_ALL)
         out.writerow(resulting.columns)
 
-        print(f'\tSaving data on the path ../data/{group_id}/data.csv')
-        resulting.to_csv(join('../data', group_id) + '/data.csv')
+        print(f'\tSaving data on the path {args.save_path}/{group_id}/data.csv')
+        resulting.to_csv(join(args.save_path, group_id) + '/data.csv')
 
 
 if __name__ == '__main__':
